a4b02c6071b732eb87cc80d35452a24192ec123e
Goran Martinjak <goran@netgen.hr>
2019-05-21 12:01:54 +0200

    remove auto open browser from start script

--- scripts/start.js
+++ scripts/start.js
@@ -114,7 +114,7 @@ checkBrowsers(paths.appPath, isInteractive)
         clearConsole();
       }
       console.log(chalk.cyan('Starting the development server...\n'));
-      openBrowser(urls.localUrlForBrowser);
+      // openBrowser(urls.localUrlForBrowser);
     });
 
     ['SIGINT', 'SIGTERM'].forEach(function(sig) {
