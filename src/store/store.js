af6c297493be9cb02534411b8ffc3a65f4819bed
Goran Martinjak <goran@netgen.hr>
2019-05-21 12:26:29 +0200

    don't use redux logger if cypress testing

--- src/store/store.js
+++ src/store/store.js
@@ -5,7 +5,8 @@ import reducer from './reducers';
 
 let middleware = [thunkMiddleware];
 if (process.env.NODE_ENV !== 'production') {
-  middleware = [...middleware, logger];
+  middleware = [...middleware];
+  if (!window.Cypress) middleware.push(logger);
 }
 export default function configureStore() {
   const store = createStore(reducer, applyMiddleware(...middleware));
