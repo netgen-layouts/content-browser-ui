3cdf334507e9d1e4329a89913322832d0fd8b525
Edi Modrić <edi.modric@gmail.com>
2019-05-21 14:06:25 +0200

    Revert "Remove index.html from the production build folder"
    This reverts commit ec4bd484b262dd043e29bfd8c368ae0ab05221a9.

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -465,13 +465,29 @@ module.exports = function(webpackEnv) {
     },
     plugins: [
       // Generates an `index.html` file with the <script> injected.
-      !isEnvProduction && new HtmlWebpackPlugin(
+      new HtmlWebpackPlugin(
         Object.assign(
           {},
           {
             inject: true,
             template: paths.appHtml,
-          }
+          },
+          isEnvProduction
+            ? {
+                minify: {
+                  removeComments: true,
+                  collapseWhitespace: true,
+                  removeRedundantAttributes: true,
+                  useShortDoctype: true,
+                  removeEmptyAttributes: true,
+                  removeStyleLinkTypeAttributes: true,
+                  keepClosingSlash: true,
+                  minifyJS: true,
+                  minifyCSS: true,
+                  minifyURLs: true,
+                },
+              }
+            : undefined
         )
       ),
       // Inlines the webpack runtime script. This script is too small to warrant
