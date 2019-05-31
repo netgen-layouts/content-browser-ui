5445c5823c7448f76ee4f335f2eca93b15e76ee3
Goran Martinjak <goran@netgen.hr>
2019-05-31 15:09:34 +0200

    add missing public path

--- config/paths.js
+++ config/paths.js
@@ -1,4 +1,31 @@
 const path = require('path');
+const fs = require('fs');
+const url = require('url');
+
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+
+const envPublicUrl = process.env.PUBLIC_URL;
+const getPublicUrl = appPackageJson =>
+  envPublicUrl || require(appPackageJson).homepage;
+
+function ensureSlash(inputPath, needsSlash) {
+  const hasSlash = inputPath.endsWith('/');
+  if (hasSlash && !needsSlash) {
+    return inputPath.substr(0, inputPath.length - 1);
+  } else if (!hasSlash && needsSlash) {
+    return `${inputPath}/`;
+  } else {
+    return inputPath;
+  }
+}
+
+function getServedPath(appPackageJson) {
+  const publicUrl = getPublicUrl(appPackageJson);
+  const servedUrl =
+    envPublicUrl || (publicUrl ? url.parse(publicUrl).pathname : '/');
+  return ensureSlash(servedUrl, true);
+}
 
 module.exports = {
   dotenv: path.resolve('.env'),
@@ -9,4 +36,6 @@ module.exports = {
   appPackageJson: path.resolve('package.json'),
   appSrc: path.resolve('src'),
   proxySetup: path.resolve('src/setupProxy.js'),
+  publicUrl: getPublicUrl(resolveApp('package.json')),
+  servedPath: getServedPath(resolveApp('package.json')),
 };
